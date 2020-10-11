c21fa7d3ff1ba6bd43d4cfb456f69f03303acf26
kimokwoo <okwookim34@gmail.com>
2020-10-11 19:47:23 +0900

    Create DrawerCallbacks.java
    서랍메뉴 인터페이스

---
+++ app/java/com/smart/school/Main/activity/DrawerCallbacks.java
@@ -0,0 +1,8 @@
+package com.smart.school.Main.activity;
+
+/**
+ * Created by d-codepages on 11/11/2014.
+ */
+public interface DrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
